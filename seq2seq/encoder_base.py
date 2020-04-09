76b02f611c702fbb21b008894ffdfd2dc3f0d943
MyungHoon Jin <jinmang2@naver.com>
2020-04-09 17:28:37 +0900

    금일 작업 내용 업로드 - 200409

--- seq2seq/encoder_base.py
+++ seq2seq/encoder_base.py
@@ -1,244 +1,178 @@
-from typing import Tuple, Union, Optional, Callable, Any
+from typing import Optional, Tuple, List, Callable, Union
+import os
+import json
+import codecs
+import collections
+
 import torch
-from torch.nn.utils.rnn import pack_padded_sequence, PackedSequence
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torch.nn.utils.rnn import PackedSequence
+from torch.nn.utils.rnn import pad_packed_sequence
+from torch.nn.utils.rnn import pack_padded_sequence
 
 
 RnnState = Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]
 RnnStateStorage = Tuple[torch.Tensor, ...]
 
 
-class _EncoderBase(torch.nn.Module):
+class _RNNEncoderBase(nn.Module):
 
-    def __init__(self, stateful: bool = False) -> None:
-        super().__init__()
+    def __init__(self, stateful: bool=False) -> None:
+        super(RNN, self).__init__()
         self.stateful = stateful
         self._states: Optional[RnnStateStorage] = None
 
-    def sort_and_run_forward(
-        self,
-        module: Callable[
-            [PackedSequence, Optional[RnnState]],
-            Tuple[Union[PackedSequence, torch.Tensor], RnnState],
-        ],
-        inputs: torch.Tensor,
-        mask: torch.BoolTensor,
-        hidden_state: Optional[RnnState] = None,
-    ):
-        """First count how many sequences are empty."""
-        batch_size = mask.size(0)
-        num_valid = torch.sum(mask[:, 0]).int().item()
+    def sort_and_run_forward(self,
+                             module: Callable[
+                                 [PackedSequence, Optional[RnnState]],
+                                 Tuple[Union[PackedSequence, torch.Tensor], RnnState]
+                             ],
+                             inputs: torch.Tensor,
+                             pad_token: int,
+                             hidden_state: Optional[RnnState] = None):
+        """input을 정렬한 후에 forward module을 돌려 output과 final hidden state를 반환"""
+        # Sorting
+        batch_size = mask.size(0) # batch_first=True
+        max_sequence_length = inputs.size(1)
+        sequence_length = (inputs != pad_token).sum(dim=-1)
+        sorted_sequence_length, permutation_index = sequence_length.sort(0, descending=True)
+        sorted_inputs = inputs.index_select(0, permutation_index)
+        _, restoration_indices = permutation_index.sort(0, descending=False)
 
-        sequence_lengths = self.get_lengths_from_binary_sequence_mask(mask)
-        (
-            sorted_inputs,
-            sorted_sequence_lengths,
-            restoration_indices,
-            sorting_indices,
-        ) = self.sort_batch_by_length(inputs, sequence_lengths)
+        # Packing
+        packed_sequence_input = pack_padded_sequence(sorted_inputs,
+                                                     sorted_sequence_length.data.tolist(),
+                                                     batch_first=True)
 
-        """Now create a PackedSequence with only the non-empty, sorted sequences."""
-        packed_sequence_input = pack_padded_sequence(
-            sorted_inputs[:num_valid, :, :],
-            sorted_sequence_lengths[:num_valid].data.tolist(),
-            batch_first=True,
-        )
-
-        """Prepare the initial states."""
+        # Prepare the initial states
         if not self.stateful:
-            if hidden_state is None:
-                initial_states: Any = hidden_state
-            elif isinstance(hidden_state, tuple):
-                initial_states = [
-                    state.index_select(1, sorting_indices)[:, :num_valid, :].contiguous()
-                    for state in hidden_state
-                ]
+            if hidden_state == None:
+                initial_states = hidden_state # Set None
+            elif isinstance(hidden_states, tuple):
+                initial_states = [state.index_select(1, sorting_indices)
+                                  for state in hidden_state]
             else:
-                initial_states = hidden_state.index_select(1, sorting_indices)[
-                    :, :num_valid, :
-                ].contiguous()
-
+                initial_states = self._get_initial_states(
+                    batch_size, max_sequence_length, sorting_indices)
         else:
-            initial_states = self._get_initial_states(batch_size, num_valid, sorting_indices)
+            initial_states = self._get_initial_states(
+                batch_size, max_sequence_length, sorting_indices)
 
-        """Actually call the module on the sorted PackedSequence."""
+        # Actually call the module on the sorted PackedSequence
         module_output, final_states = module(packed_sequence_input, initial_states)
 
         return module_output, final_states, restoration_indices
 
-    def _get_initial_states(
-        self, batch_size: int, num_valid: int, sorting_indices: torch.LongTensor
-    ) -> Optional[RnnState]:
+    def _get_initial_states(self,
+                            batch_size: int,
+                            max_sequence_length: int,
+                            sorting_indices: torch.LongTensor) -> Optional[RnnState]:
         """
-        RNN의 초기 상태를 반환
-        추가적으로, 이 메서드는 batch의 새로운 요소의 초기 상태를 추가하기 위해 상태를 변경하여(mutate)
-            호출시 batch size를 처리
-        또한 이 메서드는
-            1. 배치의 요소 seq. length로 상태를 정렬하는 것과
-            2. pad가 끝난 row 제거도 처리
-        중요한 것은 현재의 배치 크기가 이전에 호출되었을 때보다 더 크면 이 상태를 "혼합"하는 것이다.
+        (a) RNN의 초기 상태를 반환
+        (b) batch의 새로운 요소에 대한 초기 상태를 추가하기 위해
+           메서드를 호출, 상태를 변경(mutate)하여 바뀐 batch_size를 handling
+        (c) batch의 각 요소(sentence)의 sequence 길이로 state를 정렬
+        (d) pad 작업 수행 후 행을 제거
+        (e) 이전에 호출됐을 때보다 batch_size가 크면 상태를 변경(mutate)
+            - (b)의 특별 case.
 
-        이 메서드는 (1) 처음 호출되어 아무 상태가 없는 경우 (2) RNN이 heterogeneous state를 가질 때
+        이 메서드는
+            (1) 처음 호출되어 아무 상태가 없는 경우
+            (2) RNN이 heterogeneous state를 가질 때
         의 경우를 처리해야 하기 때문에 return값이 복잡함
 
         (1) module이 처음 호출됬을 때 ``module``의 타입이 무엇이든 ``None`` 반환
         (2) Otherwise,
             - LSTM의 경우 tuple of ``torch.Tensor``
-                Cell state, Hidden state
-              shape: ``(num_layers, num_valid, state_size)``
-                 and ``(num_layers, num_valid, memory_size)``
+              shape: ``(num_layers, max_seq_len, state_size)``
+                 and ``(num_layers, max_seq_len, memory_size)``
             - GRU의 경우  single ``torch.Tensor``
-                Hidden state
-              shape: ``(num_layers, num_valid, state_size)``
+              shape: ``(num_layers, max_seq_len, state_size)``
         """
-        # We don't know the state sizes the first time calling forward,
-        # so we let the module define what it's initial hidden state looks like.
+        # (1)의 경우 처리
         if self._states is None:
             return None
 
-        # Otherwise, we have some previous states.
+        # (2)의 경우 처리
         if batch_size > self._states[0].size(1):
-            # This batch is larger than the all previous states.
-            # If so, resize the states.
+            # (e)의 경우 처리
             num_states_to_concat = batch_size - self._states[0].size(1)
             resized_states = []
-            # state has shape (num_layers, batch_size, hidden_size)
+            # state의 shape은 (num_layers, batch_size, hidden_size)
             for state in self._states:
-                # This _must_ be inside the loop because some
-                # RNNs have states with different last dimension sizes.
-                zeros = state.new_zeros(state.size(0), num_states_to_concat, state.size(2))
-                resized_states.append(torch.cat([state, zeros], 1))
+                zeros = state.data.new(state.size(0),
+                                       num_states_to_concat,
+                                       state.size(2)).fill_(0)
+                zeros = Variable(zeros) # torch 1.4.0에선 Variable과 Tensor가 같음
+                resized_states.append(torch.cat([state, zeros], dim=1))
             self._states = tuple(resized_states)
             correctly_shaped_states = self._states
-
         elif batch_size < self._states[0].size(1):
-            # This batch is smaller than the previous one.
+            # (b)의 경우 처리
             correctly_shaped_states = tuple(state[:, :batch_size, :] for state in self._states)
         else:
             correctly_shaped_states = self._states
 
-        # At this point, our states are of shape (num_layers, batch_size, hidden_size).
-        # However, the encoder uses sorted sequences and additionally removes elements
-        # of the batch which are fully padded. We need the states to match up to these
-        # sorted and filtered sequences, so we do that in the next two blocks before
-        # returning the state/s.
         if len(self._states) == 1:
-            # GRUs only have a single state. This `unpacks` it from the
-            # tuple and returns the tensor directly.
-            correctly_shaped_state = correctly_shaped_states[0]
-            sorted_state = correctly_shaped_state.index_select(1, sorting_indices)
-            return sorted_state[:, :num_valid, :].contiguous()
+            # (2)에서 GRU에 해당
+            correctly_shaped_states = correctly_shaped_states[0] # unpack from tuple
+            # (c)처리
+            sorted_state = correctly_shaped_states.index_select(1, sorting_indices)
+            return sorted_state
         else:
-            # LSTMs have a state tuple of (state, memory).
-            sorted_states = [
-                state.index_select(1, sorting_indices) for state in correctly_shaped_states
-            ]
-            return tuple(state[:, :num_valid, :].contiguous() for state in sorted_states)
+            # (2)에서 LSTM에 해당
+            sorted_states = [states.index_select(1, sorting_indices)
+                             for state in correctly_shaped_states]
+            return tuple(sorted_states)
 
-    def _update_states(
-        self, final_states: RnnStateStorage, restoration_indices: torch.LongTensor
-    ) -> None:
-        """
-        RNN forward 동작 후에 state를 update
-        새로운 state로 update하며 몇 가지 book-keeping을 실시
-        즉, 상태를 해제하고 완전히 padding된 state가 업데이트되지 않도록 함
-        마지막으로 graph가 매 batch iteration후에 gc되도록 계산 그래프에서
-        state variable을 떼어냄.
-        """
-        # TODO(Mark): seems weird to sort here, but append zeros in the subclasses.
+    def _update_states(self,
+                       final_states: RnnStateStorage,
+                       restoration_indices: torch.LongTensor) -> None:
+        # TODO(Mark): seems weird to sort here, but append zeros in the subclasses
         # which way around is best?
-        new_unsorted_states = [state.index_select(1, restoration_indices) for state in final_states]
+        new_unsorted_states = [state.index_select(1, restoration_indices)
+                               for state in final_states]
 
         if self._states is None:
-            # We don't already have states, so just set the
-            # ones we receive to be the current state.
-            self._states = tuple(state.data for state in new_unsorted_states)
+            self._states = tuple(
+                [Variable(state.data)
+                 for state in new_unsorted_states]
+            )
         else:
-            # Now we've sorted the states back so that they correspond to the original
-            # indices, we need to figure out what states we need to update, because if we
-            # didn't use a state for a particular row, we want to preserve its state.
-            # Thankfully, the rows which are all zero in the state correspond exactly
-            # to those which aren't used, so we create masks of shape (new_batch_size,),
-            # denoting which states were used in the RNN computation.
+            # 어떤 상태가 RNN 계산에 사용될 지 나타내기 위해
+            # (new_batch_size,) 크기의 mask를 생성
             current_state_batch_size = self._states[0].size(1)
             new_state_batch_size = final_states[0].size(1)
-            # Masks for the unused states of shape (1, new_batch_size, 1)
-            used_new_rows_mask = [
-                (state[0, :, :].sum(-1) != 0.0).float().view(1, new_state_batch_size, 1)
-                for state in new_unsorted_states
-            ]
+            # 사용하지 않은 state에 대한 mask (1, new_batch_size, 1)
+            used_new_rows_mask = [(state[0, :, :].sum(-1) != 0.0).float().view(
+                                  1, new_state_batch_size, 1)
+                                  for state in new_unsorted_states]
             new_states = []
             if current_state_batch_size > new_state_batch_size:
-                # The new state is smaller than the old one,
-                # so just update the indices which we used.
-                for old_state, new_state, used_mask in zip(
-                    self._states, new_unsorted_states, used_new_rows_mask
-                ):
-                    # zero out all rows in the previous state
-                    # which _were_ used in the current state.
+                for old_state, new_state, used_mask in zip(self._states,
+                                                           new_unsorted_states,
+                                                           used_new_rows_mask):
+                    # all zero인 row(사용하지 않은 state)만 살림
                     masked_old_state = old_state[:, :new_state_batch_size, :] * (1 - used_mask)
-                    # The old state is larger, so update the relevant parts of it.
+                    # 사용하여 업데이트할 상태 + 사용하지 않은 상태로 기존 상태를 채움
                     old_state[:, :new_state_batch_size, :] = new_state + masked_old_state
-                    new_states.append(old_state.detach())
+                    # 계산 그래프 분리
+                    new_states.append(Variable(old_state.data))
             else:
-                # The states are the same size, so we just have to
-                # deal with the possibility that some rows weren't used.
-                new_states = []
-                for old_state, new_state, used_mask in zip(
-                    self._states, new_unsorted_states, used_new_rows_mask
-                ):
-                    # zero out all rows which _were_ used in the current state.
+                for old_state, new_state, used_mask in zip(self._states,
+                                                           new_unsorted_states,
+                                                           used_new_rows_mask):
+                    # all zero인 row(사용하지 않은 state)만 살림
                     masked_old_state = old_state * (1 - used_mask)
-                    # The old state is larger, so update the relevant parts of it.
+                    # 사용하지 않았던 상태의 값들을 이번엔 new_state에 기록
                     new_state += masked_old_state
-                    new_states.append(new_state.detach())
-
-            # It looks like there should be another case handled here - when
-            # the current_state_batch_size < new_state_batch_size. However,
-            # this never happens, because the states themeselves are mutated
-            # by appending zeros when calling _get_inital_states, meaning that
-            # the new states are either of equal size, or smaller, in the case
-            # that there are some unused elements (zero-length) for the RNN computation.
+                    # 계산 그래프 분리
+                    new_states.append(Variable(new_state))
+            # 왜 current_state_batch_size < new_state_batch_size인 경우를 고려하지 않는가?
+            # `_get_initial_state` 메서드에서 이미 처리
             self._states = tuple(new_states)
 
-    def reset_states(self, mask: torch.BoolTensor = None) -> None:
-        """
-        Resets the internal states of a stateful encoder.
-        # Parameters
-        mask : `torch.BoolTensor`, optional.
-            A tensor of shape `(batch_size,)` indicating which states should
-            be reset. If not provided, all states will be reset.
-        """
-        if mask is None:
-            self._states = None
-        else:
-            # state has shape (num_layers, batch_size, hidden_size). We reshape
-            # mask to have shape (1, batch_size, 1) so that operations
-            # broadcast properly.
-            mask_batch_size = mask.size(0)
-            mask = mask.view(1, mask_batch_size, 1)
-            new_states = []
-            for old_state in self._states:
-                old_state_batch_size = old_state.size(1)
-                if old_state_batch_size != mask_batch_size:
-                    raise ValueError(
-                        f"Trying to reset states using mask with incorrect batch size. "
-                        f"Expected batch size: {old_state_batch_size}. "
-                        f"Provided batch size: {mask_batch_size}."
-                    )
-                new_state = ~mask * old_state
-                new_states.append(new_state.detach())
-            self._states = tuple(new_states)
-
-    @staticmethod
-    def get_lengths_from_binary_sequence_mask(mask: torch.Tensor):
-        return mask.sum(-1)
-
-    @staticmethod
-    def sort_batch_by_length(tensor: torch.autograd.Variable,
-                             sequence_lengths: torch.autograd.Variable):
-        if not isinstance(tensor, Variable) or not isinstance(sequence_lengths, Variable):
-            raise Exception("Both the tensor and sequence lengths must be torch.autograd.Variables.")
-        sorted_sequence_lengths, permutation_index = sequence_lengths.sort(0, descending=True)
-        sorted_tensor = tensor.index_select(0, permutation_index)
-        _, restoration_indices = permutation_index.sort(0, descending=False)
-        return sorted_tensor, sorted_sequence_lengths, restoration_indices, permutation_index
+    def reset_states(self):
+        self._states = None
