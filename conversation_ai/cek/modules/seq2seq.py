7f5109a65ec875f087f704053c4e4ce31af3d423
MyungHoon Jin <jinmang2@naver.com>
2020-04-16 18:20:54 +0900

    누락 내용 업로드 - 200416

--- conversation_ai/cek/modules/seq2seq.py
+++ conversation_ai/cek/modules/seq2seq.py
@@ -2,35 +2,6 @@ import torch
 import torch.nn as nn
 
 
-class Highway(nn.Module):
-
-    def __init__(self,
-                 input_dim,
-                 num_layers=1,
-                 activation=nn.functional.relu):
-        super(Highway, self).__init__()
-        self.input_dim = input_dim
-        self.layers = nn.ModuleList(
-            [nn.Linear(input_dim, input_dim*2)
-             for _ in range(num_layers)]
-        )
-        self.activation = activation
-        for layer in self.layers:
-            layer.bias[input_dim:].data.fill_(1)
-
-    def forward(self, inputs):
-        current_input = x
-        for layer in self.layers:
-            projected_input = layer(current_input)
-            linear_part = current_input
-            nonlinear_part = projected_input[:, :self.input_dim]
-            gate = projected_input[:, self.input_dim:self.input_dim*2]
-            nonlinear_part = self.activation(nonlinear_part)
-            gate = torch.sigmoid(gate)
-            current_input = gate * linear_part + (1 - gate) * nonlinear_part
-        return current_input
-
-
 class Attention(nn.Module):
     pass
 
@@ -49,10 +20,11 @@ class Encoder(nn.Module):
         }
         self.biRNN = nn.LSTM(**params, bidirectional=True)
         self.uniRNN = nn.LSTM(**params, bidirectional=False)
-        self.highway = Highway(, nn.ReLU())
+        self.gate_layer = nn.Linear(hid_dim, hid_dim, bias=True)
 
     def forward(self, src):
-        pass
+        output, hidden_state = self.biRNN(src)
+
 
 
 class Decoder(nn.Module):
