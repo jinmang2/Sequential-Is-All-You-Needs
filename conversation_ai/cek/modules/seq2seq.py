b87305b63d4007df239aef914c0ae5ea50eedfcb
MyungHoon Jin <jinmang2@naver.com>
2020-04-17 21:37:45 +0900

    금일 작업 내용 업로드 - 200417

--- conversation_ai/cek/modules/seq2seq.py
+++ conversation_ai/cek/modules/seq2seq.py
@@ -10,21 +10,34 @@ class Encoder(nn.Module):
 
     def __init__(self,
                  input_dim,
-                 hid_dim):
+                 hid_dim,
+                 num_highway=1,
+                 dropout_p=0.2,
+                 bias_init=-1,
+                 activation=nn.functional.relu):
         super(Encoder, self).__init__()
-        params = {
-            'input_size' : input_dim,
-            'hidden_size': hid_dim,
-            'bias'       : True,
-            'batch_first': True,
-        }
-        self.biRNN = nn.LSTM(**params, bidirectional=True)
-        self.uniRNN = nn.LSTM(**params, bidirectional=False)
-        self.gate_layer = nn.Linear(hid_dim, hid_dim, bias=True)
+        self.biRNN = nn.LSTM(input_size=input_dim,
+                             hidden_size=hid_dim//2,
+                             bias=True,
+                             batch_first=True,
+                             bidirectional=True)
+        self.uniRNN = nn.LSTM(input_size=hid_dim,
+                              hidden_size=hid_dim,
+                              bias=True,
+                              batch_first=True,
+                              bidirectional=False)
+        self.dropout = nn.Dropout(dropout_p)
+        self.highway = nn.ModuleList(
+            [nn.Linear(hid_dim, hid_dim*2, bias=True)]
+        )
+        for layer in self.highway:
+            layer.bias.data.fill_(bias_init)
+        self.activation = activation
 
     def forward(self, src):
-        output, hidden_state = self.biRNN(src)
-
+        output, _ = self.biRNN(src)
+        output2, _ = self.uniRNN(output)
+        output + output2
 
 
 class Decoder(nn.Module):
